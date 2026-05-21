9f2d04140026aad67e427ecd4ea8cb4800057daf
Emad Jumaah <emadjumaah@gmail.com>
2026-05-21 23:41:35 +0300

    chore: untrack closed-source dirs from public repo

--- demos/pos-advanced/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { dotuix } from "@dotuix/vite-plugin";
-
-export default defineConfig({
-  plugins: [react(), dotuix()],
-});
